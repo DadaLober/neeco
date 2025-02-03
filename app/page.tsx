36945f2696df5b587d20786041c7b3e16e75d60f
Asz <126867549+DadaLober@users.noreply.github.com>
2025-02-03 13:16:54 +0800

    dockerize ms sql server

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
     <div className="flex flex-col items-center justify-center h-screen">
-      <p className="text-2xl font-bold">Neeco</p>
+      <p className="text-2xl font-bold">test</p>
     </div>
   );
 }
